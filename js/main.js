d33f8def59b5dc18aaae48edce853a07dc929899
Alfian Losari <alfianlosari@gmail.com>
2018-04-22 15:28:09 +0700

    Added Service worker caching and mobile responsive css

--- js/main.js
+++ js/main.js
@@ -5,12 +5,27 @@ var map
 var markers = []
 
 /**
+ * Service Worker Registration
+ */
+registerServiceWorker = () => {
+  if (!navigator.serviceWorker) return;
+  navigator.serviceWorker.register('../sw.js')
+    .then((reg) => console.log('registration successfull'))
+    .catch((error) => console.warn(error))
+}
+
+registerServiceWorker();
+/**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
+  
   fetchNeighborhoods();
   fetchCuisines();
 });
+
+
+
 
 /**
  * Fetch all neighborhoods and set their HTML.
@@ -143,22 +158,26 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const restaurantItemText = document.createElement('div');
+  
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  restaurantItemText.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  restaurantItemText.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  restaurantItemText.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  restaurantItemText.append(more)
+
+  li.append(restaurantItemText)
 
   return li
 }
---
+++ sw.js
@@ -0,0 +1,48 @@
+const staticCacheName = 'restaurant-review-static-v1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(staticCacheName)
+            .then((cache) => 
+                cache.addAll([
+                    'css/styles.css',
+                    'js/dbhelper.js',
+                    'js/main.js',
+                    'js/restaurant_info.js',
+                    'img/1.jpg',
+                    'img/2.jpg',
+                    'img/3.jpg',
+                    'img/4.jpg',
+                    'img/5.jpg',
+                    'img/6.jpg',
+                    'img/7.jpg',
+                    'img/8.jpg',
+                    'img/9.jpg',
+                    'img/10.jpg',
+                ])
+            )
+    );
+})
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys()
+            .then((cacheNames) => Promise.all(
+                cacheNames.filter((cacheName) => {
+                    return cacheName.startsWith('restaurant-review' && cacheName != staticCacheName);
+                }).map((cacheName) => caches.delete(cacheName))
+            )
+        )
+    )
+});
+
+self.addEventListener('fetch', (event) => {
+    const requestUrl = new URL(event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) return response;
+                return fetch(event.request)
+            })
+    )
+})
